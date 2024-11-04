8d9b95f128c0ef1ffb6c0663b431f7bdc5b4ba43
Martin <mr.mmaurer+work@gmail.com>
2024-11-04 20:16:41 +0000

    Improved validation of navcategories

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,25 @@
 import { z, defineCollection } from "astro:content";
 
+const navCategoriesInputSchema = z.record(z.string(), z.string().url());
+// type NavCategoriesInput = z.infer<typeof navCategoriesInputSchema>;
+
 const navCategories = defineCollection({
   loader: async () => {
     const response = await fetch("https://swapi.dev/api/");
-    const data = (await response.json()) as Record<string, string>;
+    const data = await response.json();
 
-    const navElements = Object.entries(data);
+    const navData = navCategoriesInputSchema.safeParse(data);
+    if (!navData.success) {
+      throw Error(navData.error.message);
+    }
 
-    return navElements.map(([id, url]) => ({
+    const navElements = Object.entries(navData.data).map(([id, url]) => ({
       id,
       url,
     }));
+
+    return navElements;
   },
-  // schema: z.record(z.string(), z.string().url())
   schema: z.object({
     id: z.string(),
     url: z.string().url(),
